76633ccbfd9060130cf3a8f12e012a75033dabeb
Paul Stone <pwillikins@Pauls-MacBook-Pro.local>
2014-03-21 09:25:51 -0600

    returns true if prime_number is passed
    created second test to check if numbers are prime
    implemented code to reflect passing test

--- lib/prime_number.rb
+++ lib/prime_number.rb
@@ -3,6 +3,12 @@ class PrimeNumber
   def prime_number?(number)
     if number % 2 == 0
       false
+    elsif number % 3 == 0
+      false
+    elsif number % 5 == 0
+      false
+    else
+      true
     end
   end
 end
--- spec/prime_number_spec.rb
+++ spec/prime_number_spec.rb
@@ -1,9 +1,16 @@
 require 'prime_number'
 
 describe PrimeNumber do
-  it "returns false if number is not a prime" do
+  it "returns false if any number passed is not a prime" do
     number = PrimeNumber.new
 
     expect(number.prime_number?(2)).to eq false
+    expect(number.prime_number?(3)).to eq false
+  end
+
+  it "returns true if number is a prime" do
+    number = PrimeNumber.new
+
+    expect(number.prime_number?(7)).to eq true
   end
 end
